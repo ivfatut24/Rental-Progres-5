9f1f215ab2e7af4840aa2b4f76e213288ca6c5b0
ivfatut24 <42790450+ivfatut24@users.noreply.github.com>
2020-06-12 06:29:06 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,15 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://localhost/project-rental/';
-$domain =  $_SERVER['HTTP_HOST'] .
-        str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = 'http://localhost/ci_pr02_rental5/';
 
-if (!empty($_SERVER['HTTPS'])) {
-    $config['base_url'] = 'https://' . $domain;
-} else {
-    $config['base_url'] = 'http://' . $domain;
-}
 /*
 |--------------------------------------------------------------------------
 | Index File
